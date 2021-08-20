9a680fcd80d435f0fdaed5ba10b196b22d5f1c4b
alokik98 <alorshsaxena12@gmail.com>
2021-08-20 17:19:43 +0530

    Added Preview Section and more

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,15 +11,37 @@ export default function TextForm(props) {
         setText(newText);
     }
     
+    const handleToLoCase = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    
+    const deleteText = () => {
+        let newText = "";
+        setText(newText);
+    }
+    
     const [text, setText] = useState("");
     
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-            <textarea value={text} onChange={handleChange} className="form-control" id="myText" rows="8"></textarea>
+        <>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                <textarea value={text} onChange={handleChange} className="form-control" id="myText" rows="8"></textarea>
+                </div>
+                <button onClick={handleToUpCase} className="btn btn-primary mx-2">Convert to UpperCase</button>
+                <button onClick={handleToLoCase} className="btn btn-primary mx-2">Convert to LowerCase</button>
+                <button onClick={deleteText} className="btn btn-primary mx-2">Clear Text</button>
+                
             </div>
-            <button onClick={handleToUpCase} className="btn btn-primary">Convert to UpperCase</button>
-        </div>
+            <div className="container">
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>You can read the text in {0.008 * text.split(" ").length} minutes.</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
